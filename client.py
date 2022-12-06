53daf7a1af7445e03cea49b2619173ebef26049d
Lê Văn Duẩn <93870205+DuanLee7662@users.noreply.github.com>
2022-12-06 12:41:20 +0700

    Update client.py

--- client.py
+++ client.py
@@ -4,3 +4,21 @@ def send_message():
         message = input("")
         client_socket.send(message.encode(ENCODER))
 
+def recieve_message():
+    '''Recieve an incoming message from the server'''
+    while True:
+        try:
+            #Recieve an incoming message from the server.
+            message = client_socket.recv(BYTESIZE).decode(ENCODER)
+
+            #Check for the name flag, else show the message
+            if message == "NAME":
+                name = input("What is your name: ")
+                client_socket.send(name.encode(ENCODER))
+            else:
+                print(message)
+        except:
+            #An error occured, close the connection
+            print("An error occured...")
+            client_socket.close()
+            break
